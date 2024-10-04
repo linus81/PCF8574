28a11690307b109cfb503f1f50720f8b5eb45c02
Giuseppe Musciacchio <linus.81@alice.it>
2024-10-04 10:53:42 +0200

    Create BasicUsage.ino

---
+++ examples/BasicUsage/BasicUsage.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include "PCF8574.h"
+
+// PCF8574 I2C address (default: 0x20)
+PCF8574 pcf;
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial);
+
+    // Initialize the PCF8574
+    Serial.println("Initializing PCF8574...");
+    if (pcf.available()) {
+        Serial.println("PCF8574 found!");
+    } else {
+        Serial.println("PCF8574 not found!");
+        while (1);
+    }
+
+    // Set all pins to OUTPUT mode
+    pcf.SetAllPinMode(OUTPUT);
+}
+
+void loop() {
+    // Toggle all pins
+    Serial.println("Toggling all pins...");
+    pcf.ToggleAll();
+    delay(1000);
+}
